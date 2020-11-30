d4e7a023f6ded7a76e9d07c3720d00005f846a87
KatalinaE <55049564+KatalinaE@users.noreply.github.com>
2020-11-30 21:40:37 +0300

    Update find-invalid-executables.c

--- posix/filesystem/find-invalid-executables.c
+++ posix/filesystem/find-invalid-executables.c
@@ -8,16 +8,18 @@
 enum { NAMESIZE = 1024, BUFSIZE = 1024 * 8 };
 
 int main() {
-  char name[NAMESIZE], buf[BUFSIZE];
+  char name[NAMESIZE], buf[BUFSIZE], elf[] = {0x7f, 'E', 'L', 'F'};
   int fd;
   struct stat st;
 
   while (scanf(" %[^\n]", name) == 1) {
-    if (!(lstat(name, &st) == 0 && st.st_mode & S_IXUSR) ||
-        (fd = open(name, O_RDONLY)) == -1 || read(fd, buf, BUFSIZE) < 4 ||
-        !strncmp(buf, "\x7f\ELF", 4) ||
-        (sscanf(buf, "#! %s", buf) == 1 && access(buf, F_OK | X_OK) == -1))
+    if ((lstat(name, &st) == 0 && st.st_mode & S_IXUSR) &&
+        ((fd = open(name, O_RDONLY)) == -1 || read(fd, buf, BUFSIZE) < 4 ||
+        strncmp(buf, elf, 4) &&
+        !(sscanf(buf, "#! %s", buf) == 1 && access(buf, F_OK | X_OK) != -1)))
       printf("%s\n", name);
+    if (fd != -1)
+      close(fd);
   }
   return 0;
 }
